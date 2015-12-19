a4e400d53a690c98a3ece1eca33c734c414da4bb
Christopher G. Green <christopher.g.green@gmail.com>
2015-12-19 01:35:39 -0800

    Continuing to refine GreenMartinSimulation.Rnw.

--- srcbuild3_hre.bat
+++ srcbuild3_hre.bat
@@ -3,13 +3,13 @@ setlocal
 set topdir=E:\cggreen\Research\dissertation2\chapters\calibration\HardinRockeExtension
 SET TMPDIR=%topdir%
 set package=%topdir%\HardinRockeExtension
-@dir HardinRockeExtension*.tar.gz /b /o-d > hre.tmp
-set /p tarball=< hre.tmp
-del hre.tmp
-rem set tarball=%topdir%\HardinRockeExtension_1.0.13.tar.gz
 rem R CMD build --help
 R CMD build %package% 
 rem R CMD INSTALL --help
+@dir HardinRockeExtension*.tar.gz /b /o-d > hre.tmp
+@set /p tarball=< hre.tmp
+@del hre.tmp
+rem set tarball=%topdir%\HardinRockeExtension_1.0.13.tar.gz
 R CMD INSTALL --library="D:\Program Files\R\R-3.1.3\site-library" %tarball%
 R CMD INSTALL --library="D:\Program Files\R\R-3.0.2\site-library" %tarball%
 R CMD INSTALL --library="D:\Program Files\R\R-2.15.2\site-library" %tarball% 
