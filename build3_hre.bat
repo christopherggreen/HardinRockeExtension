4180de2dec476630e1915c90f2c35a2022b5bdfd
Christopher G. Green <christopher.g.green@gmail.com>
2015-12-20 22:12:57 -0800

    Updating build scripts for modified repository structure.

--- build3_hre.bat
+++ build3_hre.bat
@@ -1,6 +1,6 @@
 setlocal
 (SET PATH=.;D:\Rtools31\bin;D:\perl64\bin;D:\Rtools31\gcc-4.6.3\bin;D:\miktex29\miktex\bin\x64;D:\Program Files\R\R-3.1.3\bin\x64;C:\windows;C:\windows\system32)
-set package="E:\cggreen\Research\dissertation2\chapters\calibration\HardinRockeExtension\HardinRockeExtension"
+set package="E:\cggreen\Research\dissertation2\chapters\calibration\HardinRockeExtensionWork\HardinRockeExtension"
 R CMD INSTALL --library="D:\Program Files\R\R-3.1.3\site-library" %package%
 R CMD INSTALL --library="D:\Program Files\R\R-3.0.2\site-library" %package%
 R CMD INSTALL --library="D:\Program Files\R\R-2.15.2\site-library" %package% 
