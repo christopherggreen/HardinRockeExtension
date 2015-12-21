588124f1447f4edda86ff78c8b246740eea60c91
Christopher G. Green <christopher.g.green@gmail.com>
2015-12-20 21:11:29 -0800

    Finished cleaning up GreenMartinSimulation.Rnw.

--- srcbuild3_hre.bat
+++ srcbuild3_hre.bat
@@ -3,15 +3,17 @@ setlocal
 set topdir=E:\cggreen\Research\dissertation2\chapters\calibration\HardinRockeExtension
 SET TMPDIR=%topdir%
 set package=%topdir%\HardinRockeExtension
-rem R CMD build --help
+@rem R CMD build --help
 R CMD build %package% 
-rem R CMD INSTALL --help
+IF %ERRORLEVEL% NEQ 0 GOTO ScriptEnd
+@rem R CMD INSTALL --help
 @dir HardinRockeExtension*.tar.gz /b /o-d > hre.tmp
 @set /p tarball=< hre.tmp
 @del hre.tmp
-rem set tarball=%topdir%\HardinRockeExtension_1.0.13.tar.gz
+@rem set tarball=%topdir%\HardinRockeExtension_1.0.13.tar.gz
 R CMD INSTALL --library="D:\Program Files\R\R-3.1.3\site-library" %tarball%
 R CMD INSTALL --library="D:\Program Files\R\R-3.0.2\site-library" %tarball%
-R CMD INSTALL --library="D:\Program Files\R\R-2.15.2\site-library" %tarball% 
+@rem R CMD INSTALL --library="D:\Program Files\R\R-2.15.2\site-library" %tarball% 
+:ScriptEnd
 d:\cygwin\bin\sleep 5
 endlocal
