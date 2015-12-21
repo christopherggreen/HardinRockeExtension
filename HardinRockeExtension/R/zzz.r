4f899fcb11f827884e5ebfabfc379370b435eee4
Christopher G. Green <christopher.g.green@gmail.com>
2015-12-20 22:15:34 -0800

    Removing HardinRockeExtension directory prior to readding it as a submodule.

--- HardinRockeExtension/R/zzz.r
+++
@@ -1,9 +0,0 @@
-# Miscellaneous utility functions 
-# for the 'HardinRockeExtension' package
-#
-# Christopher G. Green
-# 2014
-
-# Fraction of the sample used to compute the MCD in
-# the maximum breakdown point case
-max.bdp.mcd.alpha <- function(n,p) floor((n+p+1)/2)/n
