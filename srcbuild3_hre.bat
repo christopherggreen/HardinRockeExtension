f745bb094502dd53c7d3ea31b7918cbb50db0168
Christopher G. Green <christopher.g.green@gmail.com>
2015-12-20 22:40:15 -0800

    Fixing build script for new repository structure.

--- srcbuild3_hre.bat
+++ srcbuild3_hre.bat
@@ -1,7 +1,7 @@
 setlocal
 (SET PATH=.;D:\Rtools31\bin;D:\perl64\bin;D:\Rtools31\gcc-4.6.3\bin;D:\miktex29\miktex\bin\x64;D:\Program Files\R\R-3.1.3\bin\x64;C:\windows;C:\windows\system32)
-set topdir=E:\cggreen\Research\dissertation2\chapters\calibration\HardinRockeExtensionWork
-SET TMPDIR=%topdir%
+set topdir=E:\cggreen\Research\dissertation2\chapters\calibration
+@rem SET TMPDIR=%topdir%
 set package=%topdir%\HardinRockeExtension
 @rem R CMD build --help
 R CMD build %package% 
